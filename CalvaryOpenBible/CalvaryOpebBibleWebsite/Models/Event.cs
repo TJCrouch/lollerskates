191dd3bd91d873cf4cb594e5476aab8f08fe8d08
TJCrouch <tjcrouch90@gmail.com>
2015-04-06 15:18:13 -0700

    initial

---
+++ CalvaryOpenBible/CalvaryOpebBibleWebsite/Models/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Spatial;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CalvaryOpebBibleWebsite.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+
+        [Required]
+        public string EventName { get; set; }
+
+        [Required]
+        public string EventLocation { get; set; }
+
+        public string EventCoordinator { get; set; }
+    }
+}
---
+++ CalvaryOpenBible/CalvaryOpebBibleWebsite/Models/Video.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Spatial;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CalvaryOpebBibleWebsite.Models
+{
+    public class Video
+    {
+        public int VideoID { get; set; }
+
+        [Required]
+        public string VideoTitle { get; set; }
+
+        [Required]
+        public string VideoDetails { get; set; }
+
+        public DateTime VideoDate { get; set; }
+    }
+}
